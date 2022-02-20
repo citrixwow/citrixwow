72221b6f9b9463d7fcd685dd90e77b9e20bc5dc4
glebnq <100085624+glebnq@users.noreply.github.com>
2022-02-20 19:48:04 +0300

    Добавил скрипт

--- scripts/map.js
+++ scripts/map.js
@@ -22,6 +22,32 @@
 	map.on('click', onMapClick);
 
 
+var map = L.map('map').setView([61.240138, 46.646512], 14);
+	
+		var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+			maxZoom: 24,
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1
+		}).addTo(map);
+
+	    var popup = L.popup()
+		.setLatLng([61.240138, 46.646512])
+		.setContent('My Home.')
+		.openOn(map);
+
+		function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent('You clicked the map at ' + e.latlng.toString())
+			.openOn(map3);
+	}
+
+	map.on('click', onMapClick);
+
+
 
 var map1 = L.map('map1').setView([61.240138, 46.646512], 14);
 	
