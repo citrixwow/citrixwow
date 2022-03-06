c52f711a09a1b81af13f642ab99729ddc4d1ed28
glebnq <100085624+glebnq@users.noreply.github.com>
2022-03-06 20:03:37 +0300

    Добавил скрипт

--- scripts/map1.js
+++ scripts/map1.js
@@ -1,3 +1,4 @@
+var points = new Array(0);
 var map4 = L.map('map4').setView([61.248970, 46.645845], 14);
 	
 		var tiles4 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -13,11 +14,35 @@ var map4 = L.map('map4').setView([61.248970, 46.645845], 14);
 		.bindPopup("<b style='color:red'>Мой город</b><br/> <span style='color:blue;'>Котлас.</span>").openPopup();
 
 		function onMapClick4(e) {
-		popup
-			.setLatLng(e.latlng)
-			.setContent("<div style='color:red;'>Ты нажал на координаты </div>" + e.latlng.toString())
-			.openOn(map4);
-			console.log('Ты нажал на координаты' + e.latlng.toString());
+                if(points.length==0)
+                {
+                 var marker = L.marker(e.latlng).addTo(map4);
+                 points.push(e.latlng);
+                }
+                else
+                {
+                var marker = L.marker(e.latlng).addTo(map4);
+                points.push(e.latlng);
+                var totaldistance=Measure(points);
+                marker.bindPopup("Расстояние " + totaldistance).openPopup();
+                }
 	}
 
 	map4.on('click', onMapClick4);
+
+
+function Measure(points)
+{
+ var result=0;
+ for(var i=1;i<points.length;i++)
+  {
+  var p1=points[i-1];
+  var p2=points[i];
+  result=result+distance(p1, p2);
+  }
+return result;
+}
+function distance(p1, p2)
+{
+return 1;
+}
